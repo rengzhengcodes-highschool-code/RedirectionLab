05e03474657b652294d26384dfe09cea531811a9
rengzhengcodes <rzhengcontacts@gmail.com>
2021-01-04 13:32:16 -0500

    Implemented first letter is vowel algo.

--- PigLatin.java
+++ PigLatin.java
@@ -1,1 +1,31 @@
-public static 
+public class PigLatin {
+
+	private static boolean inArray(Object element, Object[] array) {
+		for (Object e : array) {
+			if (element.equals(e)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static String pigLatinSimple (String s) {
+		String[] vowels = {"a", "e", "i", "o", "u"};
+		String firstLetter = s.substring(0, 1);
+		boolean firstLetterIsVowel = false;
+
+		for (String vowel : vowels) {
+			if (firstLetter.equals(vowel)) {
+				firstLetterIsVowel = true;
+			}
+		}
+
+		if (firstLetterIsVowel) {
+			
+		}
+
+		return "";
+	}
+
+}
