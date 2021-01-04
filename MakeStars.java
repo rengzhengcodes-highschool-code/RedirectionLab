5170c20c732fa47e27da66fd39a993b0f8945520
rengzhengcodes <rzhengcontacts@gmail.com>
2021-01-04 13:08:07 -0500

    Created prereq files for assignment + implemented System.in proof of concept.

---
+++ MakeStars.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class MakeStars {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String print = "";
+
+		while (input.hasNextLine()) {
+			System.out.println(input.nextLine());
+			/*Scanner line = new Scanner(input.nextLine());
+
+			while(line.hasNext()) {
+				int charCount = line.next().length();
+				for (int i = 0; i < charCount; i++) {
+					print += "*";
+				}
+
+				if (line.hasNext()) {
+					print += " ";
+				}
+			}
+
+			if (input.hasNextLine()) {
+				print += "\n";
+			}*/
+		}
+
+		//System.out.println(print);
+	}
+
+}
