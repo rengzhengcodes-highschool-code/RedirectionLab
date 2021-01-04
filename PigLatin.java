c1216f5cf87424cb4ee292becdbea8a49017c526
rengzhengcodes <rzhengcontacts@gmail.com>
2021-01-04 13:54:42 -0500

    Revised first letter vowel detection to use private function.

--- PigLatin.java
+++ PigLatin.java
@@ -1,4 +1,5 @@
 public class PigLatin {
+	private static String[] vowels = {"a", "e", "i", "o", "u"};
 
 	private static boolean inArray(Object element, Object[] array) {
 		for (Object e : array) {
@@ -11,18 +12,11 @@ public class PigLatin {
 	}
 
 	public static String pigLatinSimple (String s) {
-		String[] vowels = {"a", "e", "i", "o", "u"};
 		String firstLetter = s.substring(0, 1);
-		boolean firstLetterIsVowel = false;
-
-		for (String vowel : vowels) {
-			if (firstLetter.equals(vowel)) {
-				firstLetterIsVowel = true;
-			}
-		}
+		boolean firstLetterIsVowel = inArray(firstLetter, vowels);
 
 		if (firstLetterIsVowel) {
-			
+
 		}
 
 		return "";
